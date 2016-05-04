78bb87b320388f45534283b1fc27d6c9079530e4
Phionx <shanjha.usa@gmail.com>
2016-05-04 14:50:27 -0400

    eliaswillfinishwhatwestarted

---
+++ ArrayPriorityQueue.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class ArrayPriorityQueue implements PriorityQueue  {
+    ArrayList<Comparable> data;
+
+    public ArrayPriorityQueue (){
+        data = new ArrayList<Comparable>();
+    }
+    public void add(Comparable x){
+        for(int i = 0; i < data.size(); i++){
+            if(data.get(i).compareTo(x) >= 0){
+                data.add(i, x);
+                return;
+            }
+        }
+        data.add(x);
+    }
+    public boolean isEmpty(){
+        return (data.size() == 0);
+    }
+    public Comparable peekMin(){
+        
+    }
+    public Comparable removeMin(){
+    }
+}
